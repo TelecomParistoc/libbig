c974d6f36374630e5fb179fc8852542efd67b14a
superpingu <arnaudbonetti@me.com>
2016-04-22 14:28:48 +0200

    eater

--- src/eater.c
+++ src/eater.c
@@ -85,13 +85,13 @@ static void turnEnd4() {
 }
 static void backwardFinished(struct motionElement * a) {
 	if(a) {}
-	setTargetHeading(100, turnEnd4);
+	setTargetHeading(150, turnEnd4);
 }
 static void turnEnd3() {
 	setRobotDistance(0);
 	setBrush();
 	queueSpeedChange(-0.1, NULL);
-	queueStopAt(-50, backwardFinished);
+	queueStopAt(-150, backwardFinished);
 }
 static void turnEnd2() {
 	setTargetHeading(170, turnEnd3);
