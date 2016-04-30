f68a7f9943db9833bed8f8b978ea769f0d48174f
superpingu <arnaudbonetti@me.com>
2016-04-30 20:13:26 +0200

    fixed eater

--- src/eater.c
+++ src/eater.c
@@ -179,8 +179,7 @@ static void turnBack() {
 	actionState = 3;
 	fastSpeedChange(0);
 	setMaxAcceleration(0.1);
-	setTargetHeading(125, turnEnd2);
-	startEater();
+	setTargetHeading(130, turnEnd2);
 	//setSideBlockingCallback(turnEnd2);
 }
 static void turnEnd() {
