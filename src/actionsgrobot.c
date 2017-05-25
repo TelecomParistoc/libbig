db3a0d7d7f80febe3befb5c38f6faab695be7af7
superpingu <arnaudbonetti@me.com>
2017-05-25 16:25:43 +0200

    add actions functions

---
+++ src/actionsgrobot.c
@@ -0,0 +1,34 @@
+#include <wiringPi.h>
+#include "actionsgrobot.h"
+#include <robotdriver/driver.h>
+
+void initMoteurs() {
+  wiringPiSetup();
+  pinMode(1, PWM_OUTPUT); // Motor 1
+  pinMode(7, OUTPUT); // Motor 2, lanceur de balles ?
+  initAX12(15200);
+  AX12setSpeed(143, SPEED);
+  AX12setSpeed(144, SPEED);
+}
+
+void testLanceurDeBalles() {
+  digitalWrite(7, 1);
+  waitFor(500); // NEED TESTING
+  digitalWrite(7, 0);
+}
+
+void activerLanceurDeBalles() {
+  digitalWrite(7, 1);
+  waitFor(3000); // NEED TESTING
+  digitalWrite(7, 0);
+}
+
+void expandGrobot() {
+  //deploy the cylinder with the mat
+  move_to(143, 0);
+  move_to(144, 0);
+  //make sure these angles are OK...
+
+  // expand with AX12
+  pwmWrite(1, 512); // Half value, NEED TESTING
+}
--- src/rocket.c
+++ src/rocket.c
@@ -1,8 +1,9 @@
 #include <robotdriver/driver.h>
+#include <stdlib.h>
 
 void launchRocket() {
 	AX12setTorque(0xFE, 0);
 	waitFor(1000);
 	AX12setTorque(135, 100);
-	AX12move(135, 400, NULL);
+	AX12move(135, -60, NULL);
 }
